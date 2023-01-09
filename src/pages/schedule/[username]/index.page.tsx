aefa56906e178d5bf45a007ef92d0f8dfd3e2649
Arthur Madureira <arthurmadureira10@gmail.com>
2023-01-09 13:35:21 -0300

    visual structure of the calendar component

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { ScheduleForm } from './ScheduleForm'
 import { Container, UserHeader } from './styles'
 
 interface ScheduleProps {
@@ -19,6 +20,8 @@ export default function Schedule(props: ScheduleProps) {
         <Heading size="2xl">{props.user.name}</Heading>
         <Text size="md">{props.user.bio}</Text>
       </UserHeader>
+
+      <ScheduleForm />
     </Container>
   )
 }
