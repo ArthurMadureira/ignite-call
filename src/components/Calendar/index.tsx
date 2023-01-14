3d60b0d0e977289c92f882169cbc850f3299bcb7
Arthur Madureira <arthurmadureira10@gmail.com>
2023-01-13 22:56:35 -0300

    finishing the days query

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -66,7 +67,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
         {
           params: {
             year: currentDate.get('year'),
-            month: currentDate.get('month'),
+            month: currentDate.get('month') + 1,
           },
         },
       )
@@ -120,7 +121,9 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
+          // date.get('date') === 17,
         }
       }),
       ...nextMonthFillArray.map((date) => {
